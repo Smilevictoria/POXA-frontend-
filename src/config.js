3bc28df0a24d7a0bc207a6bc092f561abf78ebdb
Victoria.White <95166075+Smilevictoria@users.noreply.github.com>
2024-09-25 21:28:24 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    apiGreeting: 'https://poxagpt_backend/greeting',
-    apiChat: 'https://poxagpt_backend/chat',
+    apiGreeting: 'https://poxagpt_backend.soselab.tw/greeting',
+    apiChat: 'https://poxagpt_backend.soselab.tw/chat',
     // apiHistory: 'https://kmamiz-bot-gpt.soselab.tw/api/history',
     // apiUser: 'https://kmamiz-bot-gpt.soselab.tw/api/user',
 };
