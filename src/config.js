1b47c9fb1b850464905555edccd4db22bb1384e4
Victoria.White <95166075+Smilevictoria@users.noreply.github.com>
2024-09-25 21:26:00 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    apiGreeting: 'http://192.168.100.241:5000/greeting',
-    apiChat: 'http://192.168.100.241:5000/chat',
+    apiGreeting: 'https://poxagpt_backend/greeting',
+    apiChat: 'https://poxagpt_backend/chat',
     // apiHistory: 'https://kmamiz-bot-gpt.soselab.tw/api/history',
     // apiUser: 'https://kmamiz-bot-gpt.soselab.tw/api/user',
 };
