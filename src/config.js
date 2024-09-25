1b4adebfe2cb05ef30ddf9cefac808f2a0f126e2
Victoria.White <95166075+Smilevictoria@users.noreply.github.com>
2024-09-25 20:58:38 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    apiGreeting: 'http://192.168.100.241:5000/greeting',
+    apiGreeting: '/greeting',
     apiChat: 'http://192.168.100.241:5000/chat',
     // apiHistory: 'https://kmamiz-bot-gpt.soselab.tw/api/history',
     // apiUser: 'https://kmamiz-bot-gpt.soselab.tw/api/user',
