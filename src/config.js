4755123fd345fe9f6934551103a183e92867f2d4
Victoria.White <victoria91718@gmail.com>
2024-11-26 00:52:44 +0800

    Updated config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const config = {
-    //  apiGreeting: 'https://poxagpt_backend.soselab.tw/greeting',
-    //  apiChat: 'https://poxagpt_backend.soselab.tw/chat',
+     apiGreeting: 'https://poxagpt_backend.soselab.tw/greeting',
+     apiChat: 'https://poxagpt_backend.soselab.tw/chat',
     // apiGreeting: 'http://127.0.0.1:5000/greeting',
     // apiChat: 'http://127.0.0.1:5000/chat',
     // apiHistory: 'https://kmamiz-bot-gpt.soselab.tw/api/history',
     // apiUser: 'https://kmamiz-bot-gpt.soselab.tw/api/user',
-    apiGreeting: 'https://poxa-backend.onrender.com/greeting',
-    apiChat: 'https://poxa-backend.onrender.com/chat',
+    // apiGreeting: 'https://poxa-backend.onrender.com/greeting',
+    // apiChat: 'https://poxa-backend.onrender.com/chat',
 };
 
 export default config;
